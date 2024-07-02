65cd6bf2d8fcf178d29dfef6f20f24162c3d4f14
Chinmaya Tewari <51325697+Lycons-rage@users.noreply.github.com>
2024-07-02 15:07:41 +0530

    Time: 23 ms (88.20%), Space: 43.2 MB (55.73%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.java
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -1,21 +1,17 @@
 class Solution {
     public int triangleNumber(int[] nums) {
-        if(nums.length < 3){
-            return 0;
-        }
         Arrays.sort(nums);
-        int first = 0;
-        int second = first+1;
-        int count = 0;
-        while(first!=nums.length-2){
-            int sum = nums[first] + nums[second];
-            for (int i = second+1; i<nums.length && nums[i] < sum; i++) {
-                count++;
-            }
-            second++;
-            if(second == nums.length-1){
-                first++;
-                second = first+1;
+        int n=nums.length;
+        int count=0;
+        for(int i=n-1;i>=1;i--){
+            int left=0,right=i-1;
+            while(left<right){
+                if(nums[left]+nums[right]>nums[i]){
+                    count+=right-left;
+                    right--;
+                }
+                else
+                left++;
             }
         }
         return count;

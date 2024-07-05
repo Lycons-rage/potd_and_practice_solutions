700e060a3fcad60b515c9cd519a017352d3b7ce4
Chinmaya Tewari <51325697+Lycons-rage@users.noreply.github.com>
2024-07-05 12:38:13 +0530

    Time: 1 ms (20.70%), Space: 43.2 MB (14.69%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,25 +1,26 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int index = -1;
+        // finding a breakpoint where the current indexed value is less than its next one
         for (int i = nums.length-2; i >=0 ; i--) {
             if(nums[i]<nums[i+1]){
                 index = i;
                 break;
             }
         }
-        
+        // if index remains -1, this means we have the last permutation so we only need to sort it
         if(index == -1){
             Arrays.sort(nums);
             return;
         }
-
+        //else we need to find the least number in the array greater than nums[index] and swap it
         for (int i = nums.length-1; i > index ; i--) {
             if(nums[i]>nums[index]){
                 swap(nums, i, index);
                 break;
             }
         }
-
+        //sort the rest of the array in ascending order and that's the answer
         Arrays.sort(nums, index+1, nums.length);
     }
 

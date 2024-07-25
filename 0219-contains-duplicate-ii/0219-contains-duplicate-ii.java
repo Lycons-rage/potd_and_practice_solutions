301c610ba56c6df481f71478e4691d1c63af9f29
Chinmaya Tewari <51325697+Lycons-rage@users.noreply.github.com>
2024-07-25 09:59:54 +0530

    Time: 21 ms (28.68%), Space: 57.9 MB (21.20%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if(k<=0){
+            return false;
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<nums.length; i++){
+            if (!map.containsKey(nums[i])){
+                map.put(nums[i], i);
+            } else {
+                if(Math.abs(map.get(nums[i])-i)<=k){
+                    return true;
+                } else {
+                    map.put(nums[i], i);
+                }
+            }
+        }
+        return false;
+    }
+}

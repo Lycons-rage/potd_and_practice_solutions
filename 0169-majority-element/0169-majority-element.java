4d734ad55c6d6bd6d86da0da4df81671bb32043d
Chinmaya Tewari <51325697+Lycons-rage@users.noreply.github.com>
2024-08-16 17:48:21 +0530

    Time: 14 ms (25.25%), Space: 49.1 MB (88.35%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        if(nums.length == 1){
+            return nums[0];
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i], 1);
+            } else {
+                map.put(nums[i], map.get(nums[i])+1);
+            }
+        }
+
+        int max_freq = 0, key = 0;
+        for(Map.Entry<Integer, Integer> e : map.entrySet()){
+            if (max_freq < e.getValue()){
+                key = e.getKey();
+                max_freq = e.getValue();
+            }
+        }
+
+        return key;
+    }
+}
